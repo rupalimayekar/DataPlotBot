9992b42195bb4ae4a92efe61d1899f78ee2a3948
Rupali <rupali@gmail.com>
2018-03-05 20:51:45 -0600

    Adding heroku procfile

---
+++ config.py
@@ -0,0 +1,10 @@
+# Twitter API Keys generated for your app
+plotbot_consumer_key = "pbsF8WJHVsowcUGBOKy1FIP90"
+plotbot_consumer_secret = "BQx5v6I0gVZFi8C2oscCeq3TjwcGV1CVtCcNma5SwcDMBU6rdf"
+plotbot_access_token = "287201959-ixtjt1eFEzzJzKWiUQJdlAohCP22h4O8vImZvqVz"
+plotbot_access_token_secret = "aNEPjygTZ3r3wKxFDgh3uqA7DCOsnKnXlxdG1fqlNYa0l"
+
+# If you wish to use this app for your bot, then you may change this to specify your twitter screen name
+BOT_STR = "@DataPlotBot Analyze:"    
+
+BOT_SCREEN_NAME = "@DataPlotBot"
